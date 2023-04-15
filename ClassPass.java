18e2eb9a29192be2d65555fe8163073e08e91ddf
Mehmet Barış Doğanoğlu <mebadogan@gmail.com>
2023-04-15 16:04:04 +0300

    Add files via upload

---
+++ ClassPass.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ClassPass {
+    public static void main(String[] args) {
+        int math, phys, turk, chem, music, totalGrade = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your math grade.");
+        math = input.nextInt();
+        System.out.println("Enter your phys grade.");
+        phys = input.nextInt();
+        System.out.println("Enter your turk grade.");
+        turk = input.nextInt();
+        System.out.println("Enter your chem grade.");
+        chem = input.nextInt();
+        System.out.println("Enter your music grade.");
+        music = input.nextInt();
+        totalGrade = (0 < math && math < 100) ? (totalGrade + math) : totalGrade;
+        totalGrade = (0 < phys && phys < 100) ? (totalGrade + phys) : totalGrade;
+        totalGrade = (0 < turk && turk < 100) ? (totalGrade + turk) : totalGrade;
+        totalGrade = (0 < chem && chem < 100) ? (totalGrade + chem) : totalGrade;
+        totalGrade = (0 < music && music < 100) ? (totalGrade + music) : totalGrade;
+        System.out.println("Your average grade is: " + totalGrade / 5);
+
+    }
+}
