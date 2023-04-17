5e9a1131cabd3e66a0a4ccc3d1cfefc35af8cba2
Mehmet Barış Doğanoğlu <mebadogan@gmail.com>
2023-04-18 02:57:47 +0300

    Add files via upload

---
+++ PrimeNumbers.java
@@ -0,0 +1,22 @@
+public class PrimeNumbers
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            int count = 0;
+            for (int j = i; j >= 1; j--)
+            {
+                if (i % j == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+}
